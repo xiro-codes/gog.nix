d1b995ec72bf9a0469d5da30bafa15d50fd15503
Travis Davis <me@tdavis.dev>
2023-04-26 18:03:00 -0500

    fix: path so i dont need to filter

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,8 @@
 {pkgs, libs,...}: let
   callPackage = libs.callPackageWith  ( pkgs // {
     inherit (libs) mkNativeGame;
-    fetchNextcloud = libs.fetchNextcloud { uname = "guest"; pword = "guest"; url="";};
+    fetchNextcloud = libs.fetchNextcloud rec { uname = "xiro49"; pword = "daringly-cramp-catacomb2"; url="https://cloud.tdavis.dev/remote.php/dav/files/${uname}";};
   });
-in {
-  absolute-drift = callPackage ./absolute-drift {};
+in with builtins; {
+  absolute-drift = callPackage ./games/absolute-drift {};
 }
