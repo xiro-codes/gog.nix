de81b8fb92807482818d6c5aa0a12d530c67dd49
Travis Davis <me@tdavis.dev>
2026-03-02 13:11:39 -0600

    feat: Update nixpkgs to nixos-unstable and remove alejandra input.

--- flake.nix
+++ flake.nix
@@ -1,21 +1,24 @@
 {
   description = "Contains GOG games packaged for use with nix";
-  inputs.alejandra.url = "github:kamadorueda/alejandra/3.0.0";
-  outputs = {
-    self,
-    nixpkgs,
-    alejandra,
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-    libs = import ./libs {inherit pkgs;};
-  in {
-    packages.${system} = import ./pkgs {inherit pkgs libs;};
-    devShells.${system}.default = pkgs.mkShell {
-      packages = [alejandra.defaultPackage.${system}];
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
+  outputs =
+    { self
+    , nixpkgs
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      libs = import ./libs { inherit pkgs; };
+    in
+    {
+      packages.${system} = import ./pkgs { inherit pkgs libs; };
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [ ];
+      };
+    };
 }
