27dc7f9d482874418e1c9f0d7514966ab38717f6
Travis Davis <me@tdavis.dev>
2026-03-03 06:42:00 -0600

    feat: Create desktop item, install icon, and adjust install phase for mkNativeGame.nix.

--- libs/mkNativeGame.nix
+++ libs/mkNativeGame.nix
@@ -12,15 +12,25 @@
    , steam-run
    , makeWrapper
    , lib
+   , makeDesktopItem
    , ...
    }:
+let
+  desktopItem = makeDesktopItem {
+    name = pname;
+    exec = pname;
+    icon = pname;
+    desktopName = pname;
+    genericName = "Game";
+    categories = [ "Game" ];
+  };
+in
 stdenvNoCC.mkDerivation {
   name = "${pname}";
   src = fetchFile { inherit pname version path sha256; };
   nativeBuildInputs = [ zip unzip makeWrapper ];
   buildInputs = [ steam-run ];
 
-  #sourceRoot = "source";
 
   unpackPhase = ''
     mkdir -p $out/share/${pname}
@@ -28,10 +38,16 @@ stdenvNoCC.mkDerivation {
     unzip -d $out/share/${pname} fixed.zip
     substituteInPlace $out/share/${pname}/data/noarch/start.sh --replace-warn chmod "#chmod"
   '';
-  buildPahse = ''
-  '';
+
   installPhase = ''
     makeWrapper ${steam-run}/bin/steam-run $out/bin/${pname} --add-flags $out/share/${pname}/data/noarch/start.sh
+    mkdir -p $out/share/applications/ 
+    cp ${desktopItem}/share/applications/* $out/share/applications
+    ICON_SOURCE="$out/share/${pname}/data/noarch/support/icon.png";
+    if [ -f "$ICON_SOURCE" ]; then
+      mkdir -p $out/share/icons/hicolor/scalable/apps
+      ln -s "$ICON_SOURCE" "$out/share/icons/hicolor/scalable/apps/${pname}.png"
+    fi
   '';
 
   fixupPhase = fixup;
