6a72a45886dcfa7ffe02624a984ece17804cf545
Travis Davis <me@tdavis.dev>
2026-03-03 11:06:12 -0600

    fix: Mark dead-cells package as broken.

--- pkgs/games/dead-cells/default.nix
+++ pkgs/games/dead-cells/default.nix
@@ -6,5 +6,9 @@ mkNativeGame
   paths = [
     { file = "dead_cells/dead_cells_1_26_0_75679.sh"; sha256 = "sha256-YDM7my8fo8gj/qp25OHyiJmSUDDuTZRIcER1VPLJ/6Q="; }
   ];
+  meta = {
+    broken = true;
+  };
 }
   inputs
+
