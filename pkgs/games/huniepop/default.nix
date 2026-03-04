e41c1af77d3fad7fd9ff8bc24366437aa6165ca9
Travis Davis <me@tdavis.dev>
2026-03-04 07:32:37 -0600

    fix: Mark huniepop as broken due to missing libGLU.so.1 dependency.

--- pkgs/games/huniepop/default.nix
+++ pkgs/games/huniepop/default.nix
@@ -7,7 +7,7 @@ mkNativeGame
     { file = "huniepop/gog_huniepop_2.0.0.3.sh"; sha256 = "sha256-fnr0qEbiqePviQrL+RRFpWy66+UgU3I4h3D/3t6QyZ4="; }
   ];
   # ./HuniePop.x86: error while loading shared libraries: libGLU.so.1: cannot open shared object file: No such file or directory
-  meta.broken = true
-    }
-    inputs
+  meta.broken = true;
+}
+  inputs
 
