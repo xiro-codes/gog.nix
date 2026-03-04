2f7409d5a335e503c69e8e5fadc9ef90e91acc62
Travis Davis <me@tdavis.dev>
2026-03-04 04:53:30 -0600

    chore: Mark hacknet as broken due to missing libnss3.so.

--- pkgs/games/hacknet/default.nix
+++ pkgs/games/hacknet/default.nix
@@ -6,5 +6,8 @@ mkNativeGame
   paths = [
     { file = "hacknet/hacknet_en_5_069_15083.sh"; sha256 = "sha256-uQaqgPj/UmzWOIpUqN6Ez3Ndn2V2DjiZbhurHMSzM70="; }
   ];
+  # ./Hacknet.bin.x86_64: error while loading shared libraries: libnss3.so: cannot open shared object file: No such file or directory
+  meta.broken = true;
 }
   inputs
+
