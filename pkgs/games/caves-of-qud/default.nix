1e695a823b856fc6b95592e61ea72d3843a531b6
Travis Davis <me@tdavis.dev>
2026-03-05 04:33:05 -0600

    chore: Update caves-of-qud to version 0.1.2 and mark as broken.

--- pkgs/games/caves-of-qud/default.nix
+++ pkgs/games/caves-of-qud/default.nix
@@ -2,10 +2,10 @@
 mkNativeGame
 {
   pname = "caves-of-qud";
-  version = "0.1.1";
+  version = "0.1.2";
   paths = [
-    { file = "caves_of_qud/caves_of_qud_2_0_210_24_89142.sh"; sha256 = "sha256-pvr+4e4uo/V7/Ebw/z4cAf4ZCCWrn49Zy70BKxUJ0ew="; }
+    { file = "caves_of_qud/caves_of_qud_2_0_210_24_89142.sh"; sha256 = "sha256-fkbqTTBJPL4qDLCCjDLTlyjSV0FJ0eHpJ3U+BE3uCqw="; }
   ];
+  meta.broken = true;
 }
   inputs
-
