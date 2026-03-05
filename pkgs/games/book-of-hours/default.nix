ba1017d07f43ed5a2a17d173e94c20b95d25282e
Travis Davis <me@tdavis.dev>
2026-03-04 20:12:48 -0600

    fix: Update book-of-hours checksum.

--- pkgs/games/book-of-hours/default.nix
+++ pkgs/games/book-of-hours/default.nix
@@ -4,7 +4,7 @@ mkNativeGame
   pname = "book-of-hours";
   version = "0.1.0";
   paths = [
-    { file = "book_of_hours/book_of_hours_2025_8_e_6_86506.sh"; sha256 = "sha256-kp9R1vcZmlgweZ12YdNQNjoyB+nl5uWp/BAqJ5NpfFg="; }
+    { file = "book_of_hours/book_of_hours_2025_8_e_6_86506.sh"; sha256 = "sha256-NJnxaP5F4f5wcMb7cQLK+Tia5O6INHhDxplzvti+Gj8="; }
   ];
   # testing this again bad dl 
   meta.broken = false;
