dfdb5a95c2947b022ca4f19d37487caae1732c35
Travis Davis <me@tdavis.dev>
2026-03-07 18:16:56 -0600

    chore: Switch to mkSimpleGame and mark HuniePop as broken.

--- pkgs/games/huniepop/default.nix
+++ pkgs/games/huniepop/default.nix
@@ -1,16 +1,12 @@
-{ mkNativeGame, ... } @ inputs:
-mkNativeGame
+{ mkSimpleGame, ... } @ inputs:
+mkSimpleGame
 {
   pname = "huniepop";
   version = "0.1.0";
   paths = [
     { file = "huniepop/gog_huniepop_2.0.0.3.sh"; sha256 = "sha256-fnr0qEbiqePviQrL+RRFpWy66+UgU3I4h3D/3t6QyZ4="; }
   ];
-  fixup = ''
-    # Fix missing libGLU.so.1
-    wrapProgram $out/bin/huniepop \
-      --prefix LD_LIBRARY_PATH : ${inputs.libGLU}/lib
-  '';
+  meta.broken = true;
 }
   inputs
 
