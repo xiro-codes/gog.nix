3d85decd08d623c214ec7e435288ed0df0d7b2bb
Travis Davis <me@tdavis.dev>
2026-03-07 18:47:27 -0600

    fix: Mark orwell package as broken and use mkSimpleGame.

--- packages/linux-games/orwell/default.nix
+++ packages/linux-games/orwell/default.nix
@@ -1,5 +1,5 @@
-{ mkNativeGame, ... } @ inputs:
-mkNativeGame
+{ mkSimpleGame, ... } @ inputs:
+mkSimpleGame
 {
   pname = "orwell";
   version = "0.1.0";
@@ -7,7 +7,7 @@ mkNativeGame
     { file = "orwell/orwell_1_4_9208_80802.sh"; sha256 = "sha256-pkEHg58/YWJ30FWi8gHgCzk6J/Lg+1w5cS16R8kioUg="; }
   ];
   # /nix/store/c4vy52jlyd3qjalaj7nc6qshif7famvc-orwell/share/orwell/data/noarch/start.sh: line 16: 239037 Segmentation fault         (core dumped) ./"Orwell.x86_64"
-  meta.broken = false;
+  meta.broken = true;
 }
   inputs
 
